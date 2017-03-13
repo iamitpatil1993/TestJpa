6a255643836d0c158d9d504e415f329e74873791
Amit Patil <amit.patil@praxify.com>
2017-03-13 12:28:27 +0530

    With Assembly plugin

---
+++ CommonResources/src/main/java/com/example/ejb/App.java
@@ -0,0 +1,13 @@
+package com.example.ejb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
--- TestEjbJpa/src/main/java/com/example/ejb/jpa/TestBeanClass.java
+++ TestEjbJpa/src/main/java/com/example/ejb/jpa/TestBeanClass.java
@@ -1,11 +1,20 @@
 package com.example.ejb.jpa;
 
 import javax.ejb.Stateless;
+
+import org.apache.log4j.Logger;
+
 import com.example.pojo.App;
 
 @Stateless
 public class TestBeanClass implements TestBean {
 
+	private static final Logger LOGGER;
+	
+	static {
+		LOGGER = Logger.getLogger(TestBeanClass.class);
+	}
+	
 	@Override
 	public void foo() {
 	
@@ -16,7 +25,7 @@ public class TestBeanClass implements TestBean {
 
 	@Override
 	public void foo2(App app) {
-		System.out.println("inside foo2");
+		LOGGER.info("inside foo2");
 		
 		if(null != app)
 			app.displayAppName();
