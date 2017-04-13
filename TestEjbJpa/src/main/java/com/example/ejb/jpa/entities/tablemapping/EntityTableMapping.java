eee254318b3c2f44c99c17160f02c85a5db85f3e
Amit Patil <amit.patil@praxify.com>
2017-04-13 08:30:18 +0530

    Table entity mapping @Table annotation in action

---
+++ TestEjbJpa/src/main/java/com/example/ejb/jpa/entities/tablemapping/EntityTableMapping.java
@@ -0,0 +1,57 @@
+package com.example.ejb.jpa.entities.tablemapping;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="table_mapping", schema="jpaTutorials")
+public class EntityTableMapping implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(name="first_name")
+	private String fname;
+	
+	@Column(name="last_name")
+	private String lname;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getFname() {
+		return fname;
+	}
+
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+
+	public String getLname() {
+		return lname;
+	}
+
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	
+	
+	
+}
